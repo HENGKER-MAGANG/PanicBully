78f30b3536e3283beaa3dc0bc5d5f3504d59257e
Ikhsan <pratamaikhsan974.com>
2025-05-11 13:00:00 +0800

    menghapus yml

--- config.php
+++ config.php
@@ -1,11 +1,8 @@
 <?php
-$host = getenv('DB_HOST');
-$dbname = getenv('DB_NAME');
-$username = getenv('DB_USER');
-$password = getenv('DB_PASSWORD');
-$port = getenv('DB_PORT') ?: 3306;
+session_start();
 
-$conn = new mysqli($host, $username, $password, $dbname, $port);
+// Koneksi DataBase
+$conn = new mysqli("localhost", "root", "", "panicbully");
 
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
