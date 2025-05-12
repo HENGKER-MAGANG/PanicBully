2b18fa05f0199ca94dd93c67aa3c6ed97c0a9c34
Ikhsan <pratamaikhsan974.com>
2025-05-12 22:24:46 +0800

    modifikasi lagiii

--- config.php
+++ config.php
@@ -2,12 +2,11 @@
 session_start();
 
 $host = getenv('DB_HOST') ?: 'db';
-$user = getenv('DB_USER') ?: 'root';
-$pass = getenv('DB_PASS') ?: '';
+$user = getenv('DB_USER') ?: 'panicuser';
+$pass = getenv('DB_PASS') ?: 'panicbully2025';
 $db   = getenv('DB_NAME') ?: 'panicbully';
 
 $conn = new mysqli($host, $user, $pass, $db);
-
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
