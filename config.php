0b9674f78479a97872ce92667fd9eb99618bf030
Ikhsan <pratamaikhsan974.com>
2025-05-12 21:48:29 +0800

    modified config dann docker-compose

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-$host = getenv('DB_HOST') ?: 'localhost';
+$host = getenv('DB_HOST') ?: 'db';  // HARUS 'db' sesuai docker-compose
 $user = getenv('DB_USER') ?: 'panicuser';
 $pass = getenv('DB_PASS') ?: 'panicbully2025!';
 $db   = getenv('DB_NAME') ?: 'panicbully';
