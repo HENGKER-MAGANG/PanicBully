9e1fb1fd54864ef78543edc6f8b0041bd8d64231
Ikhsan <pratamaikhsan974.com>
2025-05-12 22:07:06 +0800

    perbaikan

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 
-$host = getenv('DB_HOST') ?: 'db';  // HARUS 'db' sesuai docker-compose
-$user = getenv('DB_USER') ?: 'panicuser';
-$pass = getenv('DB_PASS') ?: 'panicbully2025!';
+$host = getenv('DB_HOST') ?: 'db';
+$user = getenv('DB_USER') ?: 'root';
+$pass = getenv('DB_PASS') ?: '';
 $db   = getenv('DB_NAME') ?: 'panicbully';
 
 $conn = new mysqli($host, $user, $pass, $db);
