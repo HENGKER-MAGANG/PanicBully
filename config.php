0320ac46e0f5a5f9952071ccc6ac7222629a5a24
Ikhsan <pratamaikhsan974.com>
2025-05-13 16:01:42 +0800

    edit lagiii

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$host = '127.0.0.1'; // atau IP internal MySQL jika tahu
+$host = 'mysql-panicbully';  // Gunakan nama service/database di Coolify
 $user = 'panicuser';
 $password = 'PanicUser@123';
 $database = 'default';
@@ -7,6 +7,6 @@ $database = 'default';
 $conn = mysqli_connect($host, $user, $password, $database);
 
 if (!$conn) {
-    die("Koneksi ke database gagal: " . mysqli_connect_error());
+    die("Koneksi gagal: " . mysqli_connect_error());
 }
 ?>
