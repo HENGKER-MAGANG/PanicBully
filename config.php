ffc555ba463119ff502d8698a7b82904ca140ffa
Ikhsan <pratamaikhsan974.com>
2025-05-13 15:55:41 +0800

    edit lagi

--- config.php
+++ config.php
@@ -1,17 +1,12 @@
 <?php
-$host = 'mysql-panicbully'; // atau bisa diganti sesuai IP internal/container
+$host = '127.0.0.1'; // atau IP internal MySQL jika tahu
 $user = 'panicuser';
 $password = 'PanicUser@123';
 $database = 'default';
 
-// Membuat koneksi
 $conn = mysqli_connect($host, $user, $password, $database);
 
-// Mengecek koneksi
 if (!$conn) {
     die("Koneksi ke database gagal: " . mysqli_connect_error());
 }
-
-// Jika perlu debug sukses koneksi:
-// echo "Koneksi ke database berhasil!";
 ?>
